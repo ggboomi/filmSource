124dbd412f91b3f44f49c520fa08fd8d73dae467
ggboomi <409976942@qq.com>
2017-08-05 15:58:18 +0800

    xin

---
+++ src/main/java/com/yc/fs/util/SessionAttribute.java
@@ -0,0 +1,15 @@
+package com.yc.fs.util;
+
+/**
+ * 存放session中各个属性的属性名
+ * @author navy
+ *
+ */
+public class SessionAttribute {
+	public static final String EMAILCODE = "emailCode";
+	public static final String PASSUSERNAME = "passUserNameList";
+	public static final String PASSEMAIL = "passEmailList";
+	public static final String STORETYPEINFOS = "storeType";
+	public static final String TYPESINFOS = "typesInfo";
+
+}
